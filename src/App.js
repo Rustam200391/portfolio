1769b289734e4b8e8833c06a73d6cbf9a078a899
XTS <johndoe@example.com>
2022-12-01 16:39:27 +0300

    intermedia commit build true three components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles/main.css"
 
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
-import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -12,7 +11,22 @@ function App() {
 
       <Navbar />
       <Header />
-      <Main />
+      <main className="section">
+        <div className="container">
+
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>JavaScript, TypeScript, ReactJS, Angular, HTML, CSS, NPM, BootStrap, Sass</p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Backend</h2>
+              <p>NodeJS, PHP, Laravel</p>
+            </li>
+          </ul>
+
+        </div>
+      </main>
       <Footer />
 
 
