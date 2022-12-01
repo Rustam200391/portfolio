16b0905e7eb6f2e63393a96661728ae90b9af38e
XTS <johndoe@example.com>
2022-12-01 15:20:55 +0300

    components without mistakes

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
             </button>
 
             <ul className="nav-list">
-              <li class="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-              <li class="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-              <li class="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+              <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
+              <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
+              <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
             </ul>
 
           </div>
