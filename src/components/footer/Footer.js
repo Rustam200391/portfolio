d31d19a18e5e0168d7add142112c132ead3243fd
Rustam Guseinov <rg20031991.com>
2023-04-06 15:43:04 +0300

    change social information commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
             <div className="container">
                 <div className="footer__wrapper">
                     <ul className="social">
-                        <li className="social__item"><a href="#!"><img src={vk} alt=" " /></a></li>
-                        <li className="social__item"><a href="#!"><img src={gitHub} alt=" " /></a></li>
+                        <li className="social__item"><a href="https://vk.com/rustamguseinov"><img src={vk} alt=" " /></a></li>
+                        <li className="social__item"><a href="https://github.com/Rustam200391"><img src={gitHub} alt=" " /></a></li>
                     </ul>
                     <div className="copyright">
                         <p>© 2023 frontend-i.learnhub.ru</p>
