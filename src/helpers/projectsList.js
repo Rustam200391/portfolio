270e2f86ac116b37401d545dd0fc8b6ae1885f35
Rustam Guseinov <rg20031991.com>
2023-04-09 16:17:11 +0300

    edit photo in gallery commit

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,20 +1,14 @@
 import project01 from './../img/projects/calculator1.png';
-import project01Big from './../img/projects/01-big.jpg';
 
 import project02 from './../img/projects/widget.png';
-import project02Big from './../img/projects/02-big.jpg';
 
 import project03 from './../img/projects/файл для верстки.png';
-import project03Big from './../img/projects/03-big.jpg';
 
 import project04 from './../img/projects/04.jpg';
-import project04Big from './../img/projects/04-big.jpg';
 
 import project05 from './../img/projects/05.jpg';
-import project05Big from './../img/projects/05-big.jpg';
 
 import project06 from './../img/projects/06.jpg';
-import project06Big from './../img/projects/06-big.jpg';
 
 
 const projects1 = [
@@ -22,42 +16,36 @@ const projects1 = [
         title: "CalculateJS",
         skills: "React",
         img: project01,
-        imgBig: project01Big,
         gitHubLink: 'https://github.com'
     },
     {
         title: "Weather widget",
         skills: "React",
         img: project02,
-        imgBig: project02Big,
         gitHubLink: 'https://github.com'
     },
     {
         title: "Landing №1",
         skills: "React, HTML, CSS, JS",
         img: project03,
-        imgBig: project03Big,
         gitHubLink: 'https://github.com'
     },
     {
         title: "Dating app",
         skills: "React, Typescript",
         img: project04,
-        imgBig: project04Big,
         gitHubLink: 'https://github.com'
     },
     {
         title: "Landing №2",
         skills: "HTML, CSS, JS",
         img: project05,
-        imgBig: project05Big,
         gitHubLink: 'https://github.com'
     },
     {
         title: "Gaming community",
         skills: "React, PHP, NodeJS",
         img: project06,
-        imgBig: project06Big,
         gitHubLink: 'https://github.com'
     }
 
