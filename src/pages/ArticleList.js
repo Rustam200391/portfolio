7abd71716b90db745bc479ff8bf074147d239587
Rustam Guseinov <rg20031991.com>
2023-04-11 18:50:31 +0300

    edit component ArticleList

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,38 +1,49 @@
 /* eslint-disable no-undef */
-import React, {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css" ;
 
 
+
 export const ArticleList = () => {
-  const [articlelist,setArticlelist] = useState([])
-  const fetchUsers = async () => {
-    const response = await fetch(process.env.REACT_APP_BASE_URL)
-    const articlelist = await response.json()
-    setArticlelist(articlelist)
-  }
-    useEffect(() => {
-        fetchUsers()
-    }, [])
-  
-  
-    return (
-    <div>
-        <h1>ArticleList</h1>
+   const [posts, setPosts] = useState([]);
 
-        <div>
-            {articlelist.map((userId) => {
-                const {id} = userId
-                return (
-                    <article key={id}>
-                        <title>{title}</title>
-                    </article>
-                )
-                })}
+   useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
 
-        </div>
-    </div>
-  )
-}
+   return (
+      <>
+         <div className="add-post-container">
+            <form>
+               <input type="text" className="form-control" />
+               <textarea className="form-control" cols="10" rows="8"></textarea>
+               <button type="submit">Add Post</button>
+            </form>
+         </div>
+         <div className="posts-container">
+            {posts.map((post) => {
+               return (
+                  <div className="post-card" key={post.id}>
+                     <h2 className="post-title">{post.title}</h2>
+                     <p className="post-body">{post.body}</p>
+                     <div className="button">
+                        <div className="delete-btn">Delete</div>
+                     </div>
+                  </div>
+               );
+            })}
+         </div>
+      </>
+   );
+};
 
- 
+
 
